b6ff961b91c39c07e18ca88265323fdfe3e49775
shirinkalam <mahdishiri869@gmail.com>
2022-07-13 10:17:55 +0430

    creating categories implemented

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -31,4 +31,10 @@ class CategoriesController extends Controller
 
         return back()->with('success','دسته بندی ایجاد شد');
     }
+
+    public function all()
+    {
+        $categories = Category::all();
+        return view('admin.categories.all',compact('categories'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,15 @@ use App\Http\Controllers\Admin\CategoriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function(){
+
     Route::prefix('categories')->group(function(){
-        Route::get('create',[CategoriesController::class,'create']);
+
+        Route::get('',[CategoriesController::class,'all'])->name('admin.categories.all');
+
+        Route::get('create',[CategoriesController::class,'create'])->name('admin.categories.create');
+
         Route::post('',[CategoriesController::class,'store'])->name('admin.categories.store');
+
     });
+
 });
